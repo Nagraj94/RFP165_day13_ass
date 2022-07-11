b71276302fe55d5a57fc6644c1f766f5b8934842
Ngowda94 <Nagraj94>
2022-07-11 15:52:43 +0530

    refactor code changing the max string value to 3rd position

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -37,7 +37,7 @@ public class MaximumTest<T extends Comparable<T>> {
     public static void main(String[] args) {
         Integer xInt = 4, yInt = 5, zInt = 3;
         Float xFloat = 5.5f, yFloat = 4.4f, zFloat = 6.6f ;
-        String xString = "Apple", yString = "Peach", zString = "Banana";
+        String xString = "Apple", yString = "Banana", zString = "Peach";
         MaximumTest.testMaxString(xString,yString,zString);
         new MaximumTest(xInt, yInt, zInt).maximum();
         new MaximumTest(xFloat,yFloat,zFloat).maximum();
