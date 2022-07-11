638c36b93b305e0e219854f2d28999324f1567b6
Ngowda94 <Nagraj94>
2022-07-11 15:45:49 +0530

    added string value to check max value

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -22,12 +22,25 @@ public class MaximumTest<T extends Comparable<T>> {
          System.out.printf("Max of %s, %s and %s is %s\n",x,y,z,max);
         return max;
     }
+    public static String testMaxString(String x, String y, String z){
+        String max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if (z.compareTo(max) > 0){
+            max = z;
+        }
+        System.out.printf("Max of %s, %s and %s is %s\n",x,y,z,max);
+        return max;
+    }
 
     public static void main(String[] args) {
         Integer xInt = 4, yInt = 5, zInt = 3;
         Float xFloat = 5.5f, yFloat = 4.4f, zFloat = 6.6f ;
-
+        String xString = "Peach", yString = "Apple", zString = "Banana";
+        MaximumTest.testMaxString(xString,yString,zString);
         new MaximumTest(xInt, yInt, zInt).maximum();
         new MaximumTest(xFloat,yFloat,zFloat).maximum();
+        new MaximumTest(xString,yString,zString).maximum();
     }
 }
